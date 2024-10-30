f7e7e50f6f20e3c3df94bc6c060690c822d3c073
DiegoCandela <diegocandela0128@gmail.com>
2024-10-30 10:54:07 -0500

    Navbar_Funcional

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Logo from "../img/Logo.jpg";
+import User from "../img/User.png";
 import { Icon } from '@iconify/react';
 import "./styles/Navbar.css";
 
@@ -13,21 +15,22 @@ const Navbar = () => {
         <header className="navbar">
             <div className="navbar__container">
                 <div className="navbar__logo">
-                    <img src="tu-logo.png" alt="Logo" className="navbar__logo-img" />
+                    <img src={Logo} alt="Logo" className="navbar__logo-img" />
+                    <span className="navbar__site-name">ZenBalance</span> {/* Nombre del sitio */}
                 </div>
                 <nav className={`navbar__menu ${menuActive ? "navbar__menu--active" : ""}`}>
                     <ul className="navbar__links">
                         <li><a href="#home" className="navbar__link">Inicio</a></li>
-                        <li><a href="#about" className="navbar__link">Acerca de</a></li>
-                        <li><a href="#services" className="navbar__link">Servicios</a></li>
-                        <li><a href="#contact" className="navbar__link">Contacto</a></li>
+                        <li><a href="#about" className="navbar__link">Perfil</a></li>
+                        <li><a href="#services" className="navbar__link">Recursos</a></li>
+                        <li><a href="#contact" className="navbar__link">Nosotros</a></li>
                     </ul>
                 </nav>
                 <div className="navbar__login">
-                    <img src="login-icon.png" alt="Login" className="navbar__login-img" />
+                    <img src={User} alt="Login" className="navbar__login-img" />
                 </div>
                 <div className="navbar__menu-icon" onClick={toggleMenu}>
-                    <Icon icon="ic:baseline-menu" width="30" height="30" /> {/* Aquí se utiliza el icono de menú */}
+                    <Icon icon="ic:baseline-menu" width="30" height="30" />
                 </div>
             </div>
         </header>
